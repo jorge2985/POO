4587ca1266cd7569200aa77d53bdd15c8e30e41a
Jorge <minfus2001@gmail.com>
2023-08-31 22:14:10 -0300

    Ejercicios POO

---
+++ cumple/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class Cumples
+{
+    public string nombre;
+    public string apellido;
+    public int dia;
+    public int mes;
+    public int anio;
+
+    public Cumples (int d, int m, int a, string n, string ap){
+        apellido = ap;
+        nombre = n;
+        dia = d;
+        mes = m;
+        anio = a;
+    }
+
+    public static void Recorrido(Cumples[] array)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(array[i].nombre + " nació el " + array[i].dia + "/" + array[i].mes + "/" + array[i].anio);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Cumples[] arrayCumples;
+        arrayCumples = new Cumples[5]; 
+        arrayCumples[0] = new Cumples (13, 6, 1997, "Ole", "Jaramillo");
+        arrayCumples[1] = new Cumples (24, 5, 1990, "Nico", "Deletrear");
+        arrayCumples[2] = new Cumples (26, 8, 1989, "Leo", "");
+        arrayCumples[3] = new Cumples (29, 4, 2008, "Jorge", "González");
+        arrayCumples[4] = new Cumples (29, 10, 1990, "Emma", "Espinoza");
+     
+        int cumpleMes;
+        int contador = 0;
+        
+        Recorrido(arrayCumples);
+
+        Console.WriteLine("Elige un número de mes y te diré cuántos amigos tuyos están de cumple: ");
+        cumpleMes = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (cumpleMes == arrayCumples[i].mes){
+                contador ++;
+            }
+        }
+        
+        if (contador > 0)
+        {
+            Console.WriteLine("La cantidad de amigos que cumplen en el mes " + cumpleMes + " es: " + contador);
+        } else {
+            Console.WriteLine("Nadie cumpleaños en el mes " + cumpleMes);
+        }
+        
+    }
+
+}
---
+++ ejercicio2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ejercicio2
+{
+    class Program
+    {
+        class Cumple
+        {
+            public int dia;
+            public int mes;
+            public int anio;
+            public string calculoFecha()
+            {
+                int diaActual = DateTime.Now.Date.Day;
+                int mesActual = DateTime.Now.Date.Month;
+                if(dia == diaActual && mes == mesActual)
+                {
+                    return "Hoy es tu cumpleaños ¡Feliz cumpleaños!";
+                } else {
+                    return "Hoy no es tu cumpleaños";
+                }
+            }
+            public string Edad() {
+                int anioActual = DateTime.Now.Date.Year;
+                int edad = anioActual - anio;
+                return "Tu edad es " + edad + " años";
+            }
+            public Cumple(int d, int m, int a)
+            {
+                dia = d;
+                mes = m;
+                anio = a;
+            }
+        }
+        public static void Main()
+        {
+            Cumple persona1 = new Cumple(29, 04, 1985);
+            Console.WriteLine(persona1.calculoFecha() + ". " + persona1.Edad());
+            Console.WriteLine("La fecha actual es: " + DateTime.Now.Date);
+        }
+    }
+}
