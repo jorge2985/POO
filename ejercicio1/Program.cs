47269880ef50a24752df28ce33fe13f4e3ce476f
jorge2985 <37635318+jorge2985@users.noreply.github.com>
2023-08-31 23:00:57 -0300

    Se agrega ejercicio 1

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        class Persona
+            {
+                public string nombre = "";
+                public string apellido = "";
+                public int edad = 0;
+                public string telefono = "";
+
+                public Persona(string nom, string ape, int e, string tel){
+                    nombre = nom;
+                    apellido = ape;
+                    edad = e;
+                    telefono = tel;
+                }
+
+                public void mostrar()
+                {
+                    Console.WriteLine(nombre);
+                    Console.WriteLine(apellido);
+                    Console.WriteLine(edad);
+                    Console.WriteLine(telefono);
+                }
+            }
+        static void Main(string[] args)
+        {
+            Persona primerPersona;
+            primerPersona = new Persona("Pablo", "Avellaneda", 53, "123456789");
+            primerPersona.mostrar();
+        }
+    }
+}
